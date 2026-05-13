c4d1299b22ec609e2cabc6c1b30a2d1550a7bbe5
souley0503 <sleynemr@gmail.com>
2026-05-13 15:15:34 +0000

    premier commit

---
+++ src/bookstoread/BookShelf.java
@@ -0,0 +1,8 @@
+package bookstoread;
+import java.util.Collections;
+import java.util.List;
+public class BookShelf {
+    public List<String> books() {
+        return Collections.emptyList();
+    }
+}
